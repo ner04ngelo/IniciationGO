2e40727a374eebc8146c833edd23d69ef3e3cc05
Unknown <jassonporras@gmail.com>
2018-09-25 16:55:20 -0600

    Added typ string and float
    Se agregó el tipo string y el tipo float

--- Main.go
+++ Main.go
@@ -2,26 +2,22 @@ package main
 
 import (
 	"fmt"
-	"unsafe"
+	"strconv"
 )
 
 func main() {
 
-/// Casting de variables
+///String
 
-/// En Go el casting tiene que ser de forma explicito ya que no funciona como lenguajes de alto nivel.
 
-var entero8 uint8   // 1 byte
-var entero16 uint16 // 2 byte
-var entero32 uint32 // 4 byte
-var entero64 uint64 //8 byte
+var stringExample string
 
-entero8=15
-entero32=230
+stringExample= "Edad : "
+edad:=30
 
-fmt.Println(unsafe.Sizeof(entero8), unsafe.Sizeof(entero16), unsafe.Sizeof(entero32), unsafe.Sizeof(entero64))
+fmt.Println(stringExample + strconv.Itoa(edad))
+
+
+
+
 }
-
-
-
-
