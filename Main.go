35ff4ae30b08a7e5a572c8d30918857c8e47dab5
Unknown <jassonporras@gmail.com>
2018-09-26 16:17:56 -0600

    Added For | Else If
    Añadido el ciclo For y la estructura de flujo If a la documentación

--- Main.go
+++ Main.go
@@ -17,7 +17,12 @@ edad:=30
 
 fmt.Println(stringExample + strconv.Itoa(edad))
 
+i:=5
 
-
+	if i <= edad {
+		fmt.Println("Usted es menor de edad")
+	} else{
+		fmt.Println("Usted tiene 30 años")
+	}
 
 }
