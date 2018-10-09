9ddb1a253896cd0552386ddef47a740cbc8c2a0d
Unknown <jassonporras@gmail.com>
2018-10-09 16:48:26 -0600

    Added Functions and Type
    Se añadió nuevo contenido al Documento de word

--- Main.go
+++ Main.go
@@ -2,103 +2,196 @@ package main
 
 import "fmt"
 
+type Money int
+
+func printExample() {
+	fmt.Println("Mundo")
+}
+
+func pointerExample(c *int) {
+	*c += 100
+}
+
+func (d Money) String()string{
+	return fmt.Sprintf("$%d",d)
+}
 func main() {
-
-	/*sliceExample := make([]byte, 4 , 10)
-
-	sliceExample = []byte{'H','O','L','A'}
-
-	fmt.Println(sliceExample)
-	fmt.Printf("SliceExample: %q \n", sliceExample)*/
-
-	/*var y []int
-
-	for i := 1; i < 13; i++ {
-		y = append(y, i)
-		fmt.Println("Array")
-		fmt.Println(y)
-		fmt.Printf("Longitud y: %d, Capacidad y: %d, Elementos y: %d \n", len(y), cap(y), i)
-	}*/
-
-	/*	sliceOrigin := []int{1, 2, 3}
-		sliceDestiny := []int{4, 5}
-		copy(sliceDestiny, sliceOrigin)
-		fmt.Println(sliceOrigin, sliceDestiny)*/
-	/*
-		objects := []string{
-			"Mesa",
-			"Silla",
-			"Ventilador",
-			"Licuadora",
-		}
+	/// Crear un propio tipo
 
 
-		for _, obj := range objects{
-			fmt.Printf("El objeto es %q \n", obj )
-		}*/
 
-	/*maps := make(map[string]string)
-	fmt.Println(maps)
 
-	mapExample := make(map[string]string, 2)
-	fmt.Println(mapExample)
 
-	maps["Nombre"] = "Paola"
-	maps["Edad"] = "20"
+	/*///Punteros
 
-	fmt.Println(maps["Nombre"])
-	fmt.Println(maps["Edad"])*/
+	a := 25
+	fmt.Println("Valor de a:", a)
+	fmt.Println("Dirección de memoria de a:", &a)
 
-	/*fmt.Println(printExample())*/
+	b := &a
 
-	/*multi := multiplicar
+	fmt.Println("Valor de b:", b)
+	fmt.Println("B contiene: ", *b)
 
-	fmt.Println(multi(1,2,3,4))*/
 
-	/*///Ejemplo de closure
-	result := 0
-	numbers := [...]int{
-		1,
-		2,
-		4,
-		5,
-		6,
+
+	pointerExample(b)
+
+	fmt.Println("Valor de a:", a)
+	fmt.Println("B contiene: ", *b)*/
+
+	//defer multiplicar(1, 2, 5)
+	//printName()
+
+	//// Leer archivos
+
+	/*f, err := os.Open("/Users/USER/Desktop/texto.txt")
+
+	if err != nil {
+		panic(err)
 	}
 
-	suma := func() {
-		for _, numero := range numbers {
-			result += numero
-		}
+	data := make([]byte, 20)
 
-		fmt.Printf("La suma es : %d \n", result)
+	c, err := f.Read(data)
+
+	if err != nil {
+		panic(err)
 	}
 
-	suma()
+	fmt.Printf(" Cantidad de bytes leidos: %d\n Texto leido: %q\n error: %v", c, data, err)
 
-	if suma != nil {
-		fmt.Println("Suma tiene implementación")
-	}*/
-
-	/*inc := incremementar()
-
-	fmt.Println("Valor de i: ", inc())
-	fmt.Println("Valor de i: ", inc())
-	fmt.Println("Valor de i: ", inc())
-	fmt.Println("Valor de i: ", inc())*/
-
-	n, v := retornoMultiple()
-
-	fmt.Printf("Número: %d , Cadena: %q ", n, v)
+	f.Close()*/
+	//defer printExample()
+	//fmt.Println("Hola")
+	//
+	//defer func() {
+	//	cadena := recover()
+	//	fmt.Println(cadena)
+	//}()
+	//panic("Error")
 
 }
 
-func retornoMultiple() (numero int, cadena string) {
+/*func multiplicar(numbers ...int8) {
+	result := 1
+
+	for _, number := range numbers {
+		result *= int(number)
+	}
+
+	fmt.Println(result)
+}*/
+
+/*func printName() {
+	fmt.Println("Ejemplo de defer")
+}*/
+
+//func main() {
+//
+//	//sliceExample := make([]byte, 4 , 10)
+//	//
+//	//sliceExample = []byte{'H','O','L','A'}
+//	//
+//	//fmt.Println(sliceExample)
+//	//fmt.Printf("SliceExample: %q \n", sliceExample)*/
+//	//
+//	///*var y []int
+//	//
+//	//for i := 1; i < 13; i++ {
+//	//	y = append(y, i)
+//	//	fmt.Println("Array")
+//	//	fmt.Println(y)
+//	//	fmt.Printf("Longitud y: %d, Capacidad y: %d, Elementos y: %d \n", len(y), cap(y), i)
+//	//}*/
+//	//
+//	///*	sliceOrigin := []int{1, 2, 3}
+//	//	sliceDestiny := []int{4, 5}
+//	//	copy(sliceDestiny, sliceOrigin)
+//	//	fmt.Println(sliceOrigin, sliceDestiny)*/
+//	///*
+//	//	objects := []string{
+//	//		"Mesa",
+//	//		"Silla",
+//	//		"Ventilador",
+//	//		"Licuadora",
+//	//	}
+//	//
+//	//
+//	//	for _, obj := range objects{
+//	//		fmt.Printf("El objeto es %q \n", obj )
+//	//	}*/
+//	//
+//	///*maps := make(map[string]string)
+//	//fmt.Println(maps)
+//	//
+//	//dias := map[int]string{
+//	// 1: "Lunes",
+//	// 2: "Martes",
+//	// 3: "Miercoles",
+//	//}
+//	//
+//	//fmt.Println(dias)
+//	//
+//	//mapExample := make(map[string]string, 2)
+//	//fmt.Println(mapExample)
+//	//
+//	//maps["Nombre"] = "Paola"
+//	//maps["Edad"] = "20"
+//	//
+//	//fmt.Println(maps["Nombre"])
+//	//fmt.Println(maps["Edad"])*/
+//	//
+//	///*fmt.Println(printExample())*/
+//	//
+//	///*multi := multiplicar
+//	//
+//	//fmt.Println(multi(1,2,3,4))*/
+//	//
+//	///*///Ejemplo de closure
+//	//result := 0
+//	//numbers := [...]int{
+//	//	1,
+//	//	2,
+//	//	4,
+//	//	5,
+//	//	6,
+//	//}
+//	//
+//	//suma := func() {
+//	//	for _, numero := range numbers {
+//	//		result += numero
+//	//	}
+//	//
+//	//	fmt.Printf("La suma es : %d \n", result)
+//	//}
+//	//
+//	//suma()
+//	//
+//	//if suma != nil {
+//	//	fmt.Println("Suma tiene implementación")
+//	//}*/
+//	//
+//	///*inc := incremementar()
+//	//
+//	//fmt.Println("Valor de i: ", inc())
+//	//fmt.Println("Valor de i: ", inc())
+//	//fmt.Println("Valor de i: ", inc())
+//	//fmt.Println("Valor de i: ", inc())*/
+//	//
+//	///*n, v := retornoMultiple()
+//	//
+//	//fmt.Printf("Número: %d , Cadena: %q ", n, v)
+//
+//}
+
+/*func retornoMultiple() (numero int, cadena string) {
 	numero = 1
 	cadena = "Hola"
 
 	return
 
-}
+}*/
 
 /*func incremementar() func() int {
 	i := 0
@@ -107,16 +200,6 @@ func retornoMultiple() (numero int, cadena string) {
 		i += 2
 		return
 	}
-}*/
-
-/*func multiplicar(numbers ...int8) int {
-	result := 1
-
-	for _, number := range numbers {
-		result *= int(number)
-	}
-
-	return result
 }*/
 
 /*func printExample() (name string) {
