b9bb2f771c311ed2d7607cf00aaf7e8d13451234
Unknown <jassonporras@gmail.com>
2019-01-12 15:54:58 -0600

    Added Select

--- Main.go
+++ Main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"time"
+)
 
 func main() {
-	fmt.Println("hola")
+	done := time.After(20 * time.Second)
+	eco := make(chan []byte)
+
+	go leerEntrada(eco)
+
+	for {
+		select {
+		case datos := <-eco:
+			os.Stdout.Write(datos)
+		case <-done:
+			fmt.Println("Se terminó el tiempo")
+			os.Exit(0)
+		}
+	}
+}
+
+func leerEntrada(out chan<- []byte) {
+	for {
+		datos := make([]byte, 1024)
+		n, _ := os.Stdin.Read(datos)
+		if n > 0 {
+			out <- datos
+		}
+	}
+
 }
